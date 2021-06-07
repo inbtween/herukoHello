1ae8286491e89368ad7d3fb01d7f9de230051a5f
Michelle Smith <smith.michelle.a@gmail.com>
2021-06-07 13:32:37 -0400

    make it better

--- src/main/java/com/company/helloheroku/controller/HelloHerokuController.java
+++ src/main/java/com/company/helloheroku/controller/HelloHerokuController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloHerokuController {
 
+    private static String name = "Michelle";
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public static String greeting() {
@@ -13,7 +15,7 @@ public class HelloHerokuController {
     }
 
     @RequestMapping(value = "/hi/{name}", method = RequestMethod.GET)
-    public String doHi(@PathVariable String name ) {
+    public String sayHi(@PathVariable String name ) {
         return "What's happening, " + name;
     }
 }
